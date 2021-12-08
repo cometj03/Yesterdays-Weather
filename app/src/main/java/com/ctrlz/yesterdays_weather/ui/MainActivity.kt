ec892a14b5c001256d6a5214cff076dc73c440e0
정해성 <harryjung1001@gmail.com>
2021-12-09 00:08:30 +0900

    extension functions

--- app/src/main/java/com/ctrlz/yesterdays_weather/ui/MainActivity.kt
+++ app/src/main/java/com/ctrlz/yesterdays_weather/ui/MainActivity.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.ctrlz.yesterdays_weather.data.CurrentWeatherData
+import com.ctrlz.yesterdays_weather.data.HistoricalWeatherData
 import com.ctrlz.yesterdays_weather.databinding.ActivityMainBinding
 import com.ctrlz.yesterdays_weather.network.RetrofitInstance
 import com.ctrlz.yesterdays_weather.network.safeApiCall
+import com.ctrlz.yesterdays_weather.util.getDateFormat
+import com.ctrlz.yesterdays_weather.util.getSecondTimestamp
+import com.ctrlz.yesterdays_weather.util.moveDay
+import com.ctrlz.yesterdays_weather.util.toDate
 import kotlinx.coroutines.Dispatchers
 import retrofit2.HttpException
 import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +33,12 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launchWhenCreated {
             val result = safeApiCall(Dispatchers.IO) {
-                RetrofitInstance.currentWeatherService.getCurrentWeather("Seoul")
+                // RetrofitInstance.currentWeatherService.getCurrentWeather("Seoul")
+                val time = Date().moveDay(-1).getSecondTimestamp()
+                Log.d(TAG, "onCreate: $time")
+                // TODO: fix the error code 400
+                RetrofitInstance.historicalWeatherService.getBeforeWeather(37.61, 126.91, time)
+//                RetrofitInstance.historicalWeatherService.getBeforeWeather(10.1, 10.1, time)
             }
 
             val response = result.getOrElse {
@@ -40,11 +52,20 @@ class MainActivity : AppCompatActivity() {
 
             if (response.isSuccessful) {
                 response.body()?.let {
-                    binding.tvTest.text = it.toString()
+                    //binding.tvTest.text = it.toString()
+                    setWeather(it)
                 } ?: Log.e(TAG, "Body is null")
             } else {
+                binding.tvTest.text = "Response not successful. code: ${response.code()}"
                 Log.e(TAG, "Response not successful. code: ${response.code()}")
             }
         }
+    }
+
+    fun setWeather(weather: HistoricalWeatherData) {
+        binding.tvTest.text = """
+            날짜: ${weather.dataPointWeather.dt.toDate().getDateFormat()}
+            기온: ${weather.dataPointWeather.temp}도
+        """.trimIndent()
     }
 }
--- app/src/main/java/com/ctrlz/yesterdays_weather/util/Extensions.kt
+++ app/src/main/java/com/ctrlz/yesterdays_weather/util/Extensions.kt
@@ -1,8 +1,21 @@
 package com.ctrlz.yesterdays_weather.util
 
+import java.text.SimpleDateFormat
 import java.util.*
 
 fun Int.toDate(): Date {
+    val milli = this * 1000  // Second to Millis
+    return Date(milli.toLong())
+}
 
-    return Date()
+fun Date.getDateFormat() = SimpleDateFormat("yyyy-MM-dd").format(this)
+
+fun Date.getSecondTimestamp() = (time / 1000).toInt()
+
+// yesterday -> delta = -1
+fun Date.moveDay(delta: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.DATE, delta)
+    return calendar.time
 }
